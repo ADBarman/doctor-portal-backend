2bbc768da333a79fed88cebffb716da856817d29
ADBarman <aumitav2017@gmail.com>
2020-04-10 00:07:49 +0600

    make fill better

--- index.js
+++ index.js
@@ -43,20 +43,58 @@ app.get('/bookedAppointments', (req, res) => {
 })
 
 //Post 
-app.post('/makeBooking' , (req,res) => {
-    const data = req.body;
-    console.log(data);
+// Updating Booking Status
+app.post('/updateBookingStatus', (req, res) => {
+    const ap = req.body;
+    console.log(ap);
     client = new MongoClient(uri, {useNewUrlParser : true,  useUnifiedTopology: true });
-    client.connect(conErr => {
-        console.log(conErr);
-        const collection = client.db('doctorsPortal').collection('bookedAppointments');
-        collection.insertOne(data, (err , result) => {
-            err ? res.status(500).send({message : err}) : res.send(result.ops[0])
-            console.log(err);
-        })
-    })
-    client.close();
 
+    client.connect(err => {
+        const collection = client.db('doctorsPortal').collection('bookedAppointments');
+        collection.updateOne(
+            { _id:ObjectId(ap.id) }, 
+            {
+            $set: {  "status" : ap.status },
+            },
+          (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err })
+            }
+            else {
+                res.send(result);
+                console.log(result);
+            }
+            client.close();
+        })
+    });
+})
+
+// Updating Prescription Status
+app.post('/updatePrescription', (req, res) => {
+    const ap = req.body;
+    console.log(ap);
+    client = new MongoClient(uri, {useNewUrlParser : true,  useUnifiedTopology: true });
+
+    client.connect(err => {
+        const collection = client.db('doctorsPortal').collection('bookedAppointments');
+        collection.updateOne(
+            { _id:ObjectId(ap.id) }, 
+            {
+            $set: {  "prescription" : ap.prescription },
+            },
+          (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err })
+            }
+            else {
+                res.send(result);
+                console.log(result);
+            }
+            client.close();
+        })
+    });
 })
 
 //Route to insert Appointment Data at once
